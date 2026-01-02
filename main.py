3a3cb88697f479fac638523facbcf36013a5410f
Jason <jason.huang606@gmail.com>
2026-01-02 11:32:40 +1100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def mood_for_bot():
-    radio.raise_event(EventBusSource.MICROBIT_ID_ACCELEROMETER,
-        EventBusValue.MICROBIT_ACCELEROMETER_EVT_DATA_UPDATE)
+"""I am simulating having a conversation. The topic is how you feel.
+"""
+
+# When button A is pressed, you replied good.
 
 def on_button_pressed_a():
     basic.show_string("Good.")
@@ -8,8 +9,18 @@ def on_button_pressed_a():
     mood_for_bot()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def mood_for_bot():
+    radio.raise_event(EventBusSource.MICROBIT_ID_ACCELEROMETER,
+        EventBusValue.MICROBIT_ACCELEROMETER_EVT_DATA_UPDATE)
+
+def on_pin_pressed_p2():
+    control.reset()
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
+
+
 def on_microbit_id_accelerometer_evt_data_update():
     global mood
+    # I don't know how robot feels, so I made a condition and it's random.
     mood = Math.random_boolean()
     if mood == True:
         basic.show_string("Good.")
@@ -23,6 +34,8 @@ def on_received_string(receivedString):
     basic.show_string(receivedString)
 radio.on_received_string(on_received_string)
 
+# When button B is pressed, you replied not good.
+
 def on_button_pressed_b():
     basic.show_string("Not good.")
     basic.show_string("How about you?")
@@ -30,8 +43,9 @@ def on_button_pressed_b():
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_logo_pressed():
-    control.reset()
+    radio.send_string("How are you?")
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
 mood = False
-radio.set_group(Math.round(255 / 2))
+led.set_brightness(Math.round(255 / 2))
+radio.set_group(255)
--- main.ts
+++ main.ts
@@ -1,18 +1,23 @@
+// When button A is pressed, you replied good.
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Good.")
+    basic.showString("How about you?")
+    mood_for_bot()
+})
 function mood_for_bot () {
     radio.raiseEvent(
     EventBusSource.MICROBIT_ID_ACCELEROMETER,
     EventBusValue.MICROBIT_ACCELEROMETER_EVT_DATA_UPDATE
     )
 }
-input.onButtonPressed(Button.A, function () {
-    basic.showString("Good.")
-    basic.showString("How about you?")
-    mood_for_bot()
-})
 input.onPinPressed(TouchPin.P2, function () {
     control.reset()
 })
+/**
+ * I am simulating having a conversation. The topic is how you feel.
+ */
 control.onEvent(EventBusSource.MICROBIT_ID_ACCELEROMETER, EventBusValue.MICROBIT_ACCELEROMETER_EVT_DATA_UPDATE, function () {
+    // I don't know how robot feels, so I made a condition and it's random.
     mood = Math.randomBoolean()
     if (mood == true) {
         basic.showString("Good.")
@@ -23,6 +28,7 @@ control.onEvent(EventBusSource.MICROBIT_ID_ACCELEROMETER, EventBusValue.MICROBIT
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
+// When button B is pressed, you replied not good.
 input.onButtonPressed(Button.B, function () {
     basic.showString("Not good.")
     basic.showString("How about you?")
